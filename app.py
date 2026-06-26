0200b51afc5b4d93d272d501306cd6da7e3e75cf
Payal Priyadarshini <80968938+Payal3214@users.noreply.github.com>
2026-06-26 18:38:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
+st.set_page_config(page_title="Funnel Dashboard", layout="wide")
 st.title("Funnel Dashboard")
+
+NOTEBOOK_FILE = "End-to-End-Funnel-Drop-Off-Returns-Retention-Analysis-for-a-Fashion-E-commerce.ipynb"
+
+with open(NOTEBOOK_FILE) as f:
+    nb = nbformat.read(f, as_version=4)
+
+ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+ep.preprocess(nb)
+
+st.success("Notebook executed successfully")
